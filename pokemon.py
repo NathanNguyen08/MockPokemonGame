81561c1cb52ec57d8691fa1dfeadea29ffe768a8
Nathan Nguyen <nathan.nguyen0815@gmail.com>
2024-02-13 20:28:56 -0800

    Started adding functions for shop, home, and route_1 and called them in the pallet_town function depending on which option the user chooses

--- pokemon.py
+++ pokemon.py
@@ -7,17 +7,63 @@ from pokedex import pokedex_list
 # Initialize Variables 
 
 # Introduction 
-print("Welcome to Pokemon")
+def introduction_cutscene():
+    print("Welcome to Pokemon")
+
 
 # Choose Starter Pokemon
-starter_Pokemon = False
-while not starter_Pokemon:
-    starter = input("Choose your starter [CHARMANDER] [BULBASAUR] [SQUIRTLE] ")
+def i_choose_you():
+    starter_Pokemon = False
+    while not starter_Pokemon:
+        starter = input("Choose your starter [CHARMANDER] [BULBASAUR] [SQUIRTLE] ")
 
-    for entry in pokedex_list:  # Checks every Pokemon in Pokedex
-        if starter.capitalize() == entry.name:
-            user_pokemon_1 = Pokemon(entry.ID, entry.name, entry.type, entry.description, entry.moves)  # Creates Pokemon Object with the right settings
-            starter_Pokemon = True
+        for entry in pokedex_list:  # Checks every Pokemon in Pokedex
+            if starter.capitalize() == entry.name:
+                print(f'You chose the {entry.type}y Pokemon {entry.name}!!')
+                print("Now go out and explore the wonderful world of POKEMONN")
+                starter_Pokemon = True
+                return Pokemon(entry.ID, entry.name, entry.type, entry.description, entry.moves)  # Creates Pokemon Object with the right settings     
+
+# Home Function
+def home():
+    pass
+
+
+# Shop Function
+def shop():
+    pass
+
+# Route 1
+def route_1():
+    pass
+
+
+# Pallet Town 
+def pallet_town():
+    pallet_town_options = ["Home", "Shop", "Route1"]
+    pallet_town = True
+    while pallet_town:
+        user_option = input("Where would you like to go? [HOME] [SHOP] [ROUTE1]").capitalize()
+        if user_option in pallet_town_options:
+            if user_option == "Home":
+                home()
+            elif user_option == "Shop":
+                shop()
+            elif user_option == "Route1":
+                route_1()
+                pallet_town = False
+
+
+# Pokemon Battle
+def battle():
+    pass
+
+
+# If name is main
+if __name__ == "__main__":
+    introduction_cutscene()
+    user_pokemon_1 = i_choose_you()
+
         
 
 
